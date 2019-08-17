f14dfcc4d05b35912429ecfe74a733439693f6b0
heardMan <markyheard@gmail.com>
2019-08-17 16:19:08 -0700

    Feat: Set Up
    -Created CSS JS, and Sass directories
    -Installed & Configured gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
+'use strict'
+
+const sass = require('gulp-sass');
+const gulp = require('gulp');
+
 function defaultTask(cb) {
     // place code for your default task here
     console.log(`Gulp has been successsfully loaded.
     Default Tasks Starting`);
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
     cb();
     console.log(`Default Task Complete`)
   }
