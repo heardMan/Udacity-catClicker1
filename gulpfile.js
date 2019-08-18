3a85f33936ccb2baf7a92a13e63065c774ec9ecf
heardMan <markyheard@gmail.com>
2019-08-17 22:35:39 -0700

    Set Up : Added Auto Prefixer and Live Reloading for Development
    -Installed and configured the sass-gulp auto prefixes
    -Installed and configured browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,31 @@
 
 const sass = require('gulp-sass');
 const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
 
-function defaultTask(cb) {
-    // place code for your default task here
-    console.log(`Gulp has been successsfully loaded.
-    Default Tasks Starting`);
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
-    cb();
-    console.log(`Default Task Complete`)
+
+
+  //compile scss to css
+  function style(){
+    //declare scss source file 
+    return gulp.src('sass/**/*.scss')
+    //pass the source file to the scss compiler
+    .pipe(sass().on('error', sass.logError))
+    //declare the destination location to save the CSS
+    .pipe(gulp.dest('./css'))
   }
   
-  exports.default = defaultTask
+  function watch(){
+      browserSync.init({
+          server: {
+              baseDir: './'
+          }
+      })
+      gulp.watch('./scss/**/*.scss', style);
+      gulp.watch('./*.html').on('change', browserSync.reload);
+      gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+  }
+  
+  
+  exports.default = style;
+  exports.watch = watch
