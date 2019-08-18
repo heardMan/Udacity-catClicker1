9cf2769db5c2ebfe5ee7deda30e6201c4966dcbe
heardMan <markyheard@gmail.com>
2019-08-17 23:08:46 -0700

    Bug Fix: SCSS not Reloading
    -corrected directory name from sass to scss

--- gulpfile.js
+++ gulpfile.js
@@ -14,19 +14,24 @@ const browserSync = require('browser-sync').create();
     .pipe(sass().on('error', sass.logError))
     //declare the destination location to save the CSS
     .pipe(gulp.dest('./css'))
+    //stream changes to the browser
+    .pipe(browserSync.stream());
   }
   
+
   function watch(){
+      //start the browser sync and declare base directory
       browserSync.init({
           server: {
               baseDir: './'
           }
-      })
-      gulp.watch('./scss/**/*.scss', style);
+      });
+      //tell gulp to 
+      gulp.watch('./sass/**/*.scss', style);
       gulp.watch('./*.html').on('change', browserSync.reload);
       gulp.watch('./js/**/*.js').on('change', browserSync.reload);
   }
   
   
-  exports.default = style;
+  exports.style = style;
   exports.watch = watch
